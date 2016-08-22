5fa1499709d01c5cae1179e5975621aabf693992
jodusnodus <thomas842@hotmail.com>
2016-08-22 12:57:21 +0200

    initial commit

---
+++ app/components/Containers/AsyncExample/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+
+/* component styles */
+import s from './styles.css'
+
+class AsyncExample extends Component {
+  render() {
+    return (
+      <section className={s.root}>
+        <Helmet
+          title="async example"
+        />
+        <h1>Async page</h1>
+        example<br />
+        example<br />
+        example<br />
+        example<br />
+        example<br />
+        example<br />
+      </section>
+    )
+  }
+}
+
+export default AsyncExample
