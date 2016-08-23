765b9d23653bdf875b9a3367cfc6850097fcc0c7
jodusnodus <thomas842@hotmail.com>
2016-08-23 14:51:46 +0200

    Check if item is not already in My List

--- app/redux/reducers/myList.js
+++ app/redux/reducers/myList.js
@@ -5,10 +5,18 @@ const initialState = {
 }
 
 export default createReducer({
-  ['ADD_MY_LIST_ITEM']: (state, { payload }) => ({
-    items: [
-      ...state.items,
-      payload,
-    ]
-  }),
+  ['ADD_MY_LIST_ITEM']: (state, { payload }) => {
+    const isNotInList = state.items.filter(({mangaid}) => mangaid == payload.mangaid).length < 1
+    if(isNotInList){
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          payload,
+        ]
+      }
+    }else{
+      return state
+    }
+  },
 }, initialState)
