9811c93ea4ea04a7696dfbd3216a121d1f245228
jodusnodus <thomas842@hotmail.com>
2016-08-27 20:55:21 +0200

    Material ui  header

--- app/components/Modules/Header/index.js
+++ app/components/Modules/Header/index.js
@@ -1,7 +1,9 @@
-// Simple header component
-
 import React from 'react'
 import { browserHistory, Link, IndexLink } from 'react-router'
+import TextField from 'material-ui/TextField'
+import IconButton from 'material-ui/IconButton'
+import ActionSearch from 'material-ui/svg-icons/action/search'
+import FlatButton from 'material-ui/FlatButton'
 
 /* component styles */
 import s from './styles.scss'
@@ -9,29 +11,41 @@ import s from './styles.scss'
 export default class Header extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      searchQuery: '',
+    }
     this.handleSearch = this.handleSearch.bind(this)
+    this.handleSearchFieldChange = this.handleSearchFieldChange.bind(this)
+  }
+  handleSearchFieldChange(e){
+    this.setState({
+      searchQuery: e.target.value,
+    })
   }
   handleSearch(){
-    const q = this.refs.searchField.value
-    if(q.length > 0){
-      browserHistory.push(`/search/${q}`)
+    const searchQuery = this.state.searchQuery
+    if(searchQuery.length > 0){
+      browserHistory.push(`/search/${searchQuery}`)
     }
   }
   render(){
     return (
       <div className={s.root}>
-        <ul className={s.menu}>
-          <li>
-            <IndexLink to="/" activeClassName={s.active}>
-              Home
-            </IndexLink>
-          </li>
+        <IndexLink to="/" activeClassName={s.active}>
+          <FlatButton label="Home" />
+        </IndexLink>
 
-          <li className={s.search}>
-            <input type="text" ref="searchField"/>
-            <button onClick={this.handleSearch}>Search</button>
-          </li>
-        </ul>
+        <div className={s.search}>
+          <TextField
+            hintText="Search"
+            ref="searchField"
+            value={this.state.searchQuery}
+            onChange={this.handleSearchFieldChange}
+            />
+          <IconButton onClick={this.handleSearch}>
+            <ActionSearch/>
+          </IconButton>
+        </div>
       </div>
     )
   }
