38a54fb661c0240e0097968e92c4fc7ad9a43f7f
jodusnodus <thomas842@hotmail.com>
2016-08-31 00:41:19 +0200

    Use primary color for header

--- app/components/Modules/Header/index.js
+++ app/components/Modules/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { browserHistory, Link, IndexLink } from 'react-router'
 import TextField from 'material-ui/TextField'
 import IconButton from 'material-ui/IconButton'
@@ -8,20 +8,30 @@ import ActionHome from 'material-ui/svg-icons/action/home'
 /* component styles */
 import s from './styles.scss'
 
-const Header = () => (
-  <div className={s.root}>
-    <IndexLink to="/" activeClassName={s.active}>
-      <IconButton>
-        <ActionHome />
-      </IconButton>
-    </IndexLink>
+export default class Header extends Component {
+  static contextTypes = {
+    muiTheme: PropTypes.object.isRequired
+  };
 
-    <Link to="/search" activeClassName={s.active}>
-      <IconButton>
-        <ActionSearch/>
-      </IconButton>
-    </Link>
-  </div>
-)
+  render(){
+    const headerStyle = {
+      backgroundColor: this.context.muiTheme.appBar.color,
+    }
 
-export default Header
+    return (
+      <div className={s.root} style={headerStyle}>
+        <IndexLink to="/" activeClassName={s.active}>
+          <IconButton>
+            <ActionHome />
+          </IconButton>
+        </IndexLink>
+
+        <Link to="/search" activeClassName={s.active}>
+          <IconButton>
+            <ActionSearch/>
+          </IconButton>
+        </Link>
+      </div>
+    )
+  }
+}
