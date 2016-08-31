24d6154f84b3e6041d5a429b177220e668d45212
jodusnodus <thomas842@hotmail.com>
2016-08-31 19:49:49 +0200

    Use AppBar component for the header

--- app/components/Modules/Header/index.js
+++ app/components/Modules/Header/index.js
@@ -8,6 +8,7 @@ import ActionSearch from 'material-ui/svg-icons/action/search'
 import ActionHome from 'material-ui/svg-icons/action/home'
 import Paper from 'material-ui/Paper'
 import LinearProgress from 'material-ui/LinearProgress'
+import AppBar from 'material-ui/AppBar'
 
 /* component styles */
 import s from './styles.scss'
@@ -21,14 +22,24 @@ class Header extends Component {
     const loading = this.props.loading > 0
 
     return (
-      <Paper
-        zDepth={3}
-        className={s.root}
-        rounded={false}
-        style={{
-          backgroundColor: this.context.muiTheme.appBar.color,
-        }}
-        >
+      <div className={s.root}>
+        <AppBar
+          title="Home"
+          iconElementLeft={
+            <IndexLink to="/">
+              <IconButton>
+                <ActionHome />
+              </IconButton>
+            </IndexLink>
+          }
+          iconElementRight={
+            <Link to="/search">
+              <IconButton>
+                <ActionSearch/>
+              </IconButton>
+            </Link>
+          }
+          />
         <LinearProgress
           mode="indeterminate"
           className={s.progress}
@@ -37,18 +48,7 @@ class Header extends Component {
             backgroundColor: this.context.muiTheme.flatButton.secondaryTextColor,
           }}
           />
-        <IndexLink to="/" activeClassName={s.active}>
-          <IconButton>
-            <ActionHome />
-          </IconButton>
-        </IndexLink>
-
-        <Link to="/search" activeClassName={s.active}>
-          <IconButton>
-            <ActionSearch/>
-          </IconButton>
-        </Link>
-      </Paper>
+      </div>
     )
   }
 }
