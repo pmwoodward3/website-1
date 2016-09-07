e7d6bd7a936e6a15dd0bed3a0d2226cb8a731618
jodusnodus <thomas842@hotmail.com>
2016-09-07 20:49:56 +0200

    Update webpack & improve dev server

--- app/server/server.js
+++ app/server/server.js
@@ -1,17 +1,18 @@
 import Express from 'express'
 import http from 'http'
-import { renderFullPage } from './utils/render'
 const webpack = require('webpack')
 const webpackConfig = require('../../webpack/common.config')
 const compiler = webpack(webpackConfig)
 const app = new Express()
 const server = new http.Server(app)
 const proxy = require('http-proxy').createProxyServer({})
+const fs = require('fs')
+const path = require('path')
+
+const fullPage = fs.readFileSync(path.join(__dirname, '../../dist/index.html'))
 
 // Port for web-dev-server and server app (bundle.js, bundle.css, dist, static)
-const port = process.env.NODE_ENV === 'development'
-  ? 3000
-  : 80
+const port = 3000
 
 app.use(require('morgan')('short'))
 
@@ -21,7 +22,9 @@ if (process.env.NODE_ENV === 'development') {
   }))
 
   app.use(require('webpack-hot-middleware')(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000,
   }))
 }
 
@@ -29,19 +32,8 @@ proxy.on('error', (err, req) => {
   console.error(err, req.url)
 })
 
-// Activate proxy for session
-app.use(/\/api\/(.*)/, (req, res) => {
-  req.url = req.originalUrl
-  proxy.web(req, res, { target: 'http://localhost:3030' })
-})
-
-// Static directory for express
-app.use('/static', Express.static(__dirname + '/../../static/'))
-app.use('/dist', Express.static(__dirname + '/../../dist/'))
-
-app.get(/.*/, (req, res) => {
-  const domain = req.get('host').replace(/\:.*/, '')
-  res.end(renderFullPage('', port, domain))
+app.get('/', (req, res) => {
+  res.end(fullPage)
 })
 
 server.listen(port, () => {
