7339fd14c3a357dd60b42018db1ec6b33cf5d079
jodusnodus <thomas842@hotmail.com>
2016-09-07 19:34:01 +0200

    Change theme colors

--- app/components/Root/theme.js
+++ app/components/Root/theme.js
@@ -1,6 +1,6 @@
 import {
-  teal500, teal700,
-  amberA200,
+  indigo500, indigo700,
+  pinkA200,
   grey100, grey300, grey400, grey500,
   white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors'
@@ -8,13 +8,13 @@ import {fade} from 'material-ui/utils/colorManipulator'
 import spacing from 'material-ui/styles/spacing'
 
 export default {
-  spacing: spacing,
+  spacing,
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    primary1Color: teal500,
-    primary2Color: teal700,
+    primary1Color: indigo500,
+    primary2Color: indigo700,
     primary3Color: grey400,
-    accent1Color: amberA200,
+    accent1Color: pinkA200,
     accent2Color: grey100,
     accent3Color: grey500,
     textColor: darkBlack,
@@ -22,7 +22,7 @@ export default {
     canvasColor: white,
     borderColor: grey300,
     disabledColor: fade(darkBlack, 0.3),
-    pickerHeaderColor: teal500,
+    pickerHeaderColor: indigo500,
     clockCircleColor: fade(darkBlack, 0.07),
     shadowColor: fullBlack,
   },
