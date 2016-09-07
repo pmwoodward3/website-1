913ca80928117b2610e84fca094dbe221077874b
jodusnodus <thomas842@hotmail.com>
2016-09-07 20:02:03 +0200

    Better loading page indicator

--- app/components/Modules/Loading/index.js
+++ app/components/Modules/Loading/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import CircularProgress from 'material-ui/CircularProgress'
+
 import s from './styles.scss'
 
 const Loading = () => (
   <div className={s.root}>
-    <h1>Loading...</h1>
+    <CircularProgress size={1.5} />
   </div>
 )
 
