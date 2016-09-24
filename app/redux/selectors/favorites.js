bb226c5435f30a44bf31154a7506e573f466757c
jodusnodus <thomas842@hotmail.com>
2016-09-24 17:22:11 +0200

    Fix favorites selector

--- app/redux/selectors/favorites.js
+++ app/redux/selectors/favorites.js
@@ -1,13 +1,14 @@
 import { createSelector } from 'reselect'
 
 const mangaTableSelector = state => state.mangaTable.items
-const favoritesSelector = state => state.favorites.items
+const favoritesSelector = state => state.favorites
 
 export const favorites = createSelector(
   mangaTableSelector,
   favoritesSelector,
   (table, favorites) => ({
-    items: favorites
+    ...favorites,
+    items: favorites.items
     .map(({mangaid}) => ({
       mangaid,
       ...table[mangaid],
