6d6ec5ccd8595fb79822a95a2af9c48e9b57c556
jodusnodus <thomas842@hotmail.com>
2016-10-06 22:42:18 +0200

    Use Link component instead of onClick routing on MangaItemCard

--- app/components/Modules/MangaItemCard/index.js
+++ app/components/Modules/MangaItemCard/index.js
@@ -4,81 +4,51 @@ import { Card, CardTitle, CardActions } from 'react-mdl/lib/Card'
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys'
 import toClass from 'utils/toClass'
 
+import Link from 'components/Modules/Link'
+
 import s from './styles.scss'
 
-export class MangaItemCard extends Component {
-  static propTypes = {
-    mangaid: PropTypes.number.isRequired,
-    cover: PropTypes.string,
-    title: PropTypes.string,
-    chapter: PropTypes.string,
-    chapternum: PropTypes.number,
-    pagenum: PropTypes.number,
-    source: PropTypes.string,
-    flex: PropTypes.bool,
+export const MangaItemCard = ({
+  mangaid,
+  title,
+  cover,
+  flex,
+  chapter,
+  chapternum,
+  pagenum,
+  source,
+}) => {
+  let subtitle = ''
+  let url = `/manga/${mangaid}`
+
+  if(!isNaN(chapternum)){
+    subtitle = `Ch. ${chapternum}`
+    url = `${url}/${chapternum}`
+  }
+  if(chapter){
+    subtitle = `Ch. ${chapter}`
+  }
+  if(pagenum){
+    subtitle = `${subtitle}, p. ${pagenum}`
+    url = `${url}/${pagenum}`
+  }
+  if(source){
+    url = `${url}?source=${source}`
   }
 
-  constructor(props) {
-    super(props)
-    this.handleTouchTap = this.handleTouchTap.bind(this)
+  if(subtitle.length < 1){
+    subtitle = undefined
   }
-  handleTouchTap(){
-    const {
-      mangaid,
-      chapternum,
-      pagenum,
-      source,
-    } = this.props
 
-    let url = `/manga/${mangaid}`
-
-    if(!isNaN(chapternum)){
-      url = `${url}/${chapternum}`
-    }
-    if(pagenum){
-      url = `${url}/${pagenum}`
-    }
-    if(source){
-      url = `${url}?source=${source}`
-    }
-
-    browserHistory.push(url)
+  const fullCover = `http://mcd.iosphe.re/t/${mangaid}/1/front/a/`
+  const cardStyle = {
+    backgroundImage: `url(${fullCover}), url(${cover})`,
   }
-  render(){
-    const {
-      mangaid,
-      title,
-      cover,
-      flex,
-      chapter,
-      chapternum,
-      pagenum,
-    } = this.props
 
-    let subtitle = ''
-
-    if(!isNaN(chapternum)){
-      subtitle = `Ch. ${chapternum}`
-    }
-    if(chapter){
-      subtitle = `Ch. ${chapter}`
-    }
-    if(pagenum){
-      subtitle = `${subtitle}, p. ${pagenum}`
-    }
-
-    if(subtitle.length < 1){
-      subtitle = undefined
-    }
-
-    const fullCover = `http://mcd.iosphe.re/t/${mangaid}/1/front/a/`
-    const cardStyle = {
-      backgroundImage: `url(${fullCover}), url(${cover})`,
-    }
-
-    return (
+  return (
+    <Link to={url} className={toClass(s.root, flex && s.flex)}>
       <Card
-        className={toClass(s.root, flex && s.flex)} onClick={this.handleTouchTap}
+        className={s.card}
         style={cardStyle}
         shadow={0}
         >
@@ -90,10 +60,20 @@ export class MangaItemCard extends Component {
           </CardActions>
         )}
       </Card>
-    )
-  }
+    </Link>
+  )
 }
 
+MangaItemCard.propTypes = {
+  mangaid: PropTypes.number.isRequired,
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  chapter: PropTypes.string,
+  chapternum: PropTypes.number,
+  pagenum: PropTypes.number,
+  source: PropTypes.string,
+  flex: PropTypes.bool,
+}
 
 export default onlyUpdateForKeys([
   'mangaid',
