c7b7fd81efc3bfe801a1e1724c5ea530feb4644b
jodusnodus <thomas842@hotmail.com>
2016-10-10 18:17:51 +0200

    Change page title on Error component to error title

--- app/components/Modules/Error/index.js
+++ app/components/Modules/Error/index.js
@@ -1,10 +1,14 @@
 import React, { PropTypes } from 'react'
+import Helmet from 'react-helmet'
 
 import s from './styles.scss'
 import './sad.svg'
 
 const Error = ({title='Page not Found'}) => (
   <div className={s.root}>
+    <Helmet
+      title={title}
+      />
     <img src="/sad.svg" alt="Sad" className={s.sad}/>
     <h1 className={s.title}>{title}</h1>
   </div>
